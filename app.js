d8b72612acab0f2139875a84265f400f05d63790
Madhu1757 <gayatrimadhulika05@gmail.com>
2026-08-08 17:12:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to My Student Server');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
