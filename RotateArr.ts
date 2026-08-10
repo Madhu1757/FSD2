cfdb846d3059ae255ba8fb88013ede021b7afd62
Madhu1757 <gayatrimadhulika05@gmail.com>
2026-08-10 18:56:27 +0530

    Delete RotateArr.ts

--- RotateArr.ts
+++
@@ -1,8 +0,0 @@
-let array:number[]=[1,2,3,4,5];
-let n: number=2;
-let rotatedArray: number[]=[
-    ...array.slice(n),
-    ...array.slice(0,n)
-];
-console.log("Original Array:",array);
-console.log("Rotated Array:",rotatedArray)
